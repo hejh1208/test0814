8cdc265196d0b73200f4c46a38080cda9b41c2f2
hjh <1213462027@qq.com>
2020-08-14 11:08:24 +0800

    ceshi

---
+++ src/com/ywwl/test/test1.java
@@ -0,0 +1,14 @@
+package com.ywwl.test;
+
+public class test1 {
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+}
